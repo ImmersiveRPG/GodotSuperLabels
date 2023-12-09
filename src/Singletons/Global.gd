1f0a8ca3c2b15ab4b1bb835f4156a15c58ac6c5b
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2023-12-08 22:17:51 -0800

    Added license headers.

--- src/Singletons/Global.gd
+++ src/Singletons/Global.gd
@@ -1,3 +1,7 @@
+# Copyright (c) 2023 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+# This file is licensed under the MIT License
+# https://github.com/ImmersiveRPG/GodotSuperLabels
+
 
 extends Node
 
--- src/SuperLabel3D/super_label_3d.gd
+++ src/SuperLabel3D/super_label_3d.gd
@@ -1,3 +1,6 @@
+# Copyright (c) 2023 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+# This file is licensed under the MIT License
+# https://github.com/ImmersiveRPG/GodotSuperLabels
 
 @tool
 extends RigidBody3D
